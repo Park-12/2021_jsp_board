9a6105dcf3f6c2e701e445df6081ea73a30865f0
Park-12 <sohyunp96@gmail.com>
2021-07-15 14:32:57 +0900

    게시물 상세페이지

--- src/main/java/com/shp/exam/exam1/http/Rq.java
+++ src/main/java/com/shp/exam/exam1/http/Rq.java
@@ -95,6 +95,20 @@ public class Rq {
 		}
 		return paramValue;
 	}
+	
+	public int getIntParam(String paramName, int defaultValue) {
+		String paramValue = req.getParameter(paramName);
+
+		if (paramValue == null) {
+			return defaultValue;
+		}
+		try {
+			return Integer.parseInt(paramValue);
+		}
+		catch(NumberFormatException e) {
+			return defaultValue;
+		}
+	}
 
 	public void printf(String format, Object... args) {
 		print(Ut.f(format, args));
@@ -122,4 +136,5 @@ public class Rq {
 		printf("location.replace('%s');\n", redirectUri);
 		println("</script>");
 	}
+
 }
--- src/main/java/com/shp/exam/exam1/http/controller/UsrArticleController.java
+++ src/main/java/com/shp/exam/exam1/http/controller/UsrArticleController.java
@@ -10,12 +10,15 @@ import com.shp.exam.exam1.service.ArticleService;
 
 public class UsrArticleController extends Controller {
 	private ArticleService articleService = Container.articleService;
-	
+
 	@Override
 	public void performAction(Rq rq) {
 		switch (rq.getActionMethodName()) {
 		case "list":
 			actionShowList(rq);
+			break;
+		case "detail":
+			actionDetailList(rq);
 			break;
 		case "write":
 			actionShowWrite(rq);
@@ -29,11 +32,26 @@ public class UsrArticleController extends Controller {
 		}
 	}
 
+	private void actionDetailList(Rq rq) {
+		int id = rq.getIntParam("id", 0);
+		
+		if (id == 0) {
+			rq.historyBack("id를 입력해주세요.");
+			return;
+		}
+
+		Article article = articleService.getForPrintArticleById(id);
+
+		rq.setAttr("article", article);
+
+		rq.jsp("usr/article/detail");
+	}
+
 	private void actionShowList(Rq rq) {
 		List<Article> articles = articleService.getForPrintArticles();
-		
+
 		rq.setAttr("articles", articles);
-		
+
 		rq.jsp("usr/article/list");
 	}
 
@@ -41,27 +59,27 @@ public class UsrArticleController extends Controller {
 		String title = rq.getParam("title", "");
 		String body = rq.getParam("body", "");
 		String redirectUri = rq.getParam("redirectUri", "../article/list");
-		
+
 		if (title.length() == 0) {
 			rq.historyBack("title을 입력해주세요.");
 			return;
 		}
-		
+
 		if (body.length() == 0) {
 			rq.historyBack("body를 입력해주세요.");
 			return;
 		}
-		
+
 		ResultData writeRd = articleService.write(title, body);
 		int id = (int) writeRd.getBody().get("id");
-		
+
 		redirectUri = redirectUri.replace("[NEW_ID]", id + "");
-		
+
 		rq.replace(writeRd.getMsg(), redirectUri);
 	}
 
 	private void actionShowWrite(Rq rq) {
 		rq.jsp("usr/article/write");
 	}
-	
+
 }
